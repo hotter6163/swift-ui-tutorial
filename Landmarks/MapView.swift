4a324be3a7cfd541f5909cf98c627c77ac0da3c7
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-02 08:51:33 +0900

    finish 1.5

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 堀田祥寿 on 2023/12/02.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.116_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
