da8b7b1b51c7192b1f84ceb15da39092fba84532
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-12-02 07:30:54 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 堀田祥寿 on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
