143cbd57cd0c8cb0b48df6c7eb157f644c3fdad2
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-01-20 16:51:35 +0900

    finish 3.2.1

---
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,29 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by 堀田祥寿 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name)")
+        }
+    }
+}
+
+#Preview {
+    HikeBadge(name: "Preview Testing")
+}
---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by 堀田祥寿 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
