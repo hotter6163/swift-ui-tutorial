097596d175387fee5d80fd98107ce2b76cf98d69
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-01-20 17:51:19 +0900

    finish 3.1.4

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by 堀田祥寿 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username")
+                Spacer()
+                TextField("Username", text: $profile.username)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notification")
+            }
+            
+            Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                ForEach(Profile.Season.allCases) { season in
+                    Text(season.rawValue).tag(season)
+                }
+            }
+            
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date")
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileEditor(profile: .constant(.default))
+}
