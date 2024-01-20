37c005f13069a1e7540b11a3d384cc3d01d77aef
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-01-20 14:33:36 +0900

    finish 3.1.3

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by 堀田祥寿 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(5))
+    )
+}
